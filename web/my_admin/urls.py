ab519bb6609c83640056131266ec5cb0f05fdb88
jialiang.yang <jialiang.yang@zongmutech.com>
2022-02-24 17:16:37 +0800

    change my_admin urls and views

--- web/my_admin/urls.py
+++ web/my_admin/urls.py
@@ -3,5 +3,18 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='admin_index')
+    path('', views.index, name='admin_index'),
+    path('login/', views.adminlogin, name='adminlogin'),
+    path('logout/', views.logout, name='admin.logout'),
+    path('imagefile/', views.imagemanage, name='imagemanage'),
+    path('imageupload/', views.imageupload, name='imageupload'),
+    path('categorym/', views.categorym, name='categorym'),
+    path('newfcategory/', views.newfcategory, name='newfcategory'),
+    path('categorydelete/', views.categorydelete, name='categorydelete'),
+    path('addproduct/', views.addproduct, name='addproduct'),
+    path('productm/newproduct/', views.newproduct, name='newproduct'),
+    path('product_list/',views.admin_product_list,name='product_list'),
+    path('prodelete/', views.prodelete, name='prodelete'),
+    path('modproduct/', views.modproduct, name='modproduct'),
+    path('modproductdata/', views.modproductdata, name='modproductdata'),
 ]
--- web/my_admin/views.py
+++ web/my_admin/views.py
@@ -2,4 +2,56 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('admin')
+    return render(request, 'badmin/admin_index.html')
+
+
+def adminlogin():
+    return None
+
+
+def logout():
+    return None
+
+
+def imagemanage():
+    return None
+
+
+def imageupload():
+    return None
+
+
+def categorym():
+    return None
+
+
+def newfcategory():
+    return None
+
+
+def categorydelete():
+    return None
+
+
+def addproduct():
+    return None
+
+
+def newproduct():
+    return None
+
+
+def admin_product_list():
+    return None
+
+
+def prodelete():
+    return None
+
+
+def modproduct():
+    return None
+
+
+def modproductdata():
+    return None
