ad7366097f00ee993e65816f4e43d8f2e67b5ad5
jialiang.yang <jialiang.yang@zongmutech.com>
2022-02-25 16:30:05 +0800

    增加user and book数据库

--- web/my_admin/models.py
+++ web/my_admin/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=100)
+
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    abstract = models.TextField()
+    img_url = models.CharField(max_length=150)
+    pub_date = models.DateField()
