53c466f7f2613461e69664b929e7372650aa2ee1
jialiang.yang <jialiang.yang@zongmutech.com>
2022-03-07 17:09:48 +0800

    修改登录认证bug

--- web/my_book/UserLoginCheck.py
+++ web/my_book/UserLoginCheck.py
@@ -12,12 +12,23 @@ class LoginCheck:
 
         urllist = [reverse('bUser :login'), reverse('bUser :book_index'), reverse('bUser :register'), reverse('adminlogin')]
 
-        if re.match('/', request.path) and request.path not in urllist:
+        # if re.match('/', request.path) and request.path not in urllist:
+        #
+        #     if request.session.get('username','') == '':
+        #         url = reverse('bUser :login')
+        #         return HttpResponse(f'<script>alert("请先登录"); location.href="{url}";</script>')
 
-            if request.session.get('username','') == '':
-                url = reverse('bUser :login')
+        if re.match('/my_admin', request.path) and request.path not in urllist:
+
+            if request.session.get('adminUser','') == '':
+                url = reverse('adminlogin')
                 return HttpResponse(f'<script>alert("请先登录"); location.href="{url}";</script>')
 
+        elif re.match('/', request.path) and request.path not in urllist:
+
+            if request.session.get('username', '') == '':
+                url = reverse('bUser :login')
+                return HttpResponse(f'<script>alert("请先登录"); location.href="{url}";</script>')
 
         response = self.get_response(request)
         return response
